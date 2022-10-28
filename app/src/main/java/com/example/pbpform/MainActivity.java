534928ea2aab52d9853cf1ed9cffe257faf1618a
liemroym <liemroymarcelino@gmail.com>
2022-10-28 17:11:50 +0700

    Refactored dark mode on calendar background
    # Conflicts:
    #	.idea/gradle.xml

--- app/src/main/java/com/example/pbpform/MainActivity.java
+++ app/src/main/java/com/example/pbpform/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.pbpform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
-import android.content.res.Configuration;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -37,23 +36,6 @@ public class MainActivity extends AppCompatActivity {
         final int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-
-        int nightModeFlags =
-                getApplicationContext().getResources().getConfiguration().uiMode &
-                        Configuration.UI_MODE_NIGHT_MASK;
-        int bgDatePicker = 0;
-        switch (nightModeFlags) {
-            case Configuration.UI_MODE_NIGHT_NO:
-                bgDatePicker = android.R.color.background_light;
-                break;
-
-            case Configuration.UI_MODE_NIGHT_YES:
-                bgDatePicker = android.R.color.background_dark;
-                break;
-
-        }
-
-        int finalBgDatePicker = bgDatePicker;
         birthText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
                         MainActivity.this,
                         setListener,
                         year, month, day);
-                datePickerDialog.getWindow().setBackgroundDrawableResource(finalBgDatePicker);
                 datePickerDialog.show();
             }
         });
